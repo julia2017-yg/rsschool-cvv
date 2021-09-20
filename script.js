ae465f160d9344178b41ba628a46eb535a7c7f72
julia2017-yg <julka.getman@gmail.com>
2021-09-20 11:45:24 +0300

    feat: add stule.css

--- script.js
+++ script.js
@@ -8,3 +8,19 @@ const iconMenu = document.querySelector('.hamburger-menu');
       menuBody.classList.toggle('active');
     });
   }
+
+  $(document).ready(function () { 
+    $('.slider-items').slick({
+      dots: false,
+      slidesToShow: 3,
+      prevArrow: "<img src='assets/icons/arrow-left.svg' class='prev' alt='1'>",
+      nextArrow: "<img src='assets/icons/arrow-right.svg' class='next' alt='2'>",
+      infinite: false,
+      // speed: 300,
+      // slidesToShow: 3,
+      // slidesToScroll: 3,
+      // arrows: false,
+      // autoplay: true
+    });
+     
+    });
