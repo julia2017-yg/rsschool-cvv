16f587fc5f7b7b8664bb72134318ad01bf689c44
julia2017-yg <julka.getman@gmail.com>
2021-09-21 08:56:46 +0300

    feat: change style css

--- script.js
+++ script.js
@@ -15,27 +15,27 @@ const iconMenu = document.querySelector('.hamburger-menu');
       prevArrow: "<img src='assets/icons/arrow-left.svg' class='prev' alt='1'>",
       nextArrow: "<img src='assets/icons/arrow-right.svg' class='next' alt='2'>",
       slidesToShow: 3,
-                      slidesToScroll: 1,
-                      arrows: true,
-                      autoplay: true,
-                      autoplaySpeed: 2000,
-                      responsive: [
-                            {
-                              breakpoint: 1200,
-                              settings: {
-                                slidesToShow: 2,
-                                slidesToScroll: 1
-                              }
-                            },
-                            {
-                              breakpoint: 1008,
-                              settings: {
-                                slidesToShow: 1,
-                                slidesToScroll: 1
-                              }
-                            },
-
-                          ]
+        slidesToScroll: 1,
+        arrows: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [
+          {
+            breakpoint: 1200,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 940,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              arrows: false,
+            }
+          },
+        ]
     });
      
-    });
+  });
